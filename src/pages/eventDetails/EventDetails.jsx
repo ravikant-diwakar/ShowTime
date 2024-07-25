b6f5716e7dc3c8a8b8a7700c469a72ed9078bc41
Your Name <your-email@example.com>
2024-07-25 16:41:22 +0530

    Updated

--- src/pages/eventDetails/EventDetails.jsx
+++ src/pages/eventDetails/EventDetails.jsx
@@ -2,13 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import Lottie from "lottie-react";
 import { BsDiagram3, BsLightning } from "react-icons/bs";
 import { CiCalendarDate, CiPlay1 } from "react-icons/ci";
-import { FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import { HiOutlineSpeakerphone } from "react-icons/hi";
 import { IoTicketOutline } from "react-icons/io5";
-import { MdPersonAddAlt1 } from "react-icons/md";
 import { PiListDashesBold, PiNotebookDuotone } from "react-icons/pi";
-import { RiFacebookFill } from "react-icons/ri";
-import { TiTick } from "react-icons/ti";
 import { VscLocation } from "react-icons/vsc";
 import { Link, useParams } from "react-router-dom";
 import loadingAnimation from "../../assets/animation/animation.json";
@@ -93,13 +89,13 @@ const EventDetails = () => {
       >
         <div className="h-[500px] bg-black bg-opacity-70 relative">
           <div className="absolute top-60 left-8 md:left-36 ">
-            <h1 className="text-5xl text-white font-bold">Event Details</h1>
+            <h1 className="text-5xl text-white font-bold">Show Details</h1>
             <p className="text-xl mt-2 flex items-center gap-2 text-white">
               <Link to={"/events"}>
-                <span className="underline">Events</span>
+                <span className="underline">Show</span>
               </Link>{" "}
               <CiPlay1 className="text-pink-600" />{" "}
-              <span className="text-red-500">Event Details</span>
+              <span className="text-red-500">Show Details</span>
             </p>
           </div>
         </div>
@@ -132,8 +128,8 @@ const EventDetails = () => {
                 <div className="flex gap-3 items-center ">
                   <PiListDashesBold className="text-red-500 text-xl" />
                   <div>
-                    <p className="font-semibold">Event Type</p>
-                    <p>Web Development</p>
+                    <p className="font-semibold">Show Type</p>
+                    <p>StandUp Comedy</p>
                   </div>
                 </div>
                 <div className="flex gap-3 items-center">
@@ -181,44 +177,7 @@ const EventDetails = () => {
               {event.description}
             </p>
           </div>
-          <div className="mt-8 grid gap-8 md:gap-6 lg:gap-0 max-w-[840px] lg:grid-cols-2 items-center">
-            <div>
-              <h2 className="text-2xl font-semibold">Overview</h2>
-              <ul className="space-y-4 mt-5 text-lg font-medium">
-                <li className="flex items-center gap-2">
-                  <TiTick className="text-2xl" /> You Got Full Free Certificate.
-                </li>
-                <li className="flex items-center gap-2">
-                  <TiTick className="text-2xl" />
-                  Unlimited Coffe & Tea When U Boring.
-                </li>
-                <li className="flex items-center gap-2">
-                  <TiTick className="text-2xl" />
-                  Comfortable Seating Areas.
-                </li>
-                <li className="flex items-center gap-2">
-                  <TiTick className="text-2xl" />
-                  Wi-Fi Access.
-                </li>
-                <li className="flex items-center gap-2">
-                  <TiTick className="text-2xl" />
-                  Lunch Suspendisse In Commodo Feli.
-                </li>
-              </ul>
-            </div>
-            <div className="flex gap-8">
-              <img
-                className="w-[38%] md:w-[48%] rounded-tr-md rounded-bl-md h-52"
-                src="https://i.ibb.co/mGZ71df/pexels-rdne-stock-project-7648306.jpg"
-                alt=""
-              />
-              <img
-                className="w-[38%] md:w-[48%] rounded-tl-md rounded-br-md h-52"
-                src="https://i.ibb.co/9qNw5xs/leadership.jpg"
-                alt=""
-              />
-            </div>
-          </div>
+
           <p className="mt-8 text-gray-600 max-w-[850px]">
             Discover the latest tech trends at Tech Expo 2024. Engage with
             leaders, explore innovations, and experience cutting-edge demos.
@@ -226,167 +185,6 @@ const EventDetails = () => {
             at Tech Expo 2024. Engage with leaders, explore innovations, and
             experience cutting-edge demos. Join us for a dynamic tech showcase.
           </p>
-
-          <h2 className="text-2xl font-semibold my-8">Main Speakers</h2>
-
-          <div className="grid md:grid-cols-2 gap-6 md:gap-0 mb-8">
-            <div className="space-y-10">
-              <div className="flex items-center gap-4">
-                <img
-                  className="w-28 h-28 rounded-full"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVYTd2Hapj5Ri_KBYNwWpK-Jk9cNuOiQbp6Q&usqp=CAU"
-                  alt=""
-                />
-                <div>
-                  <h2 className="text-xl font-semibold">Foyez Ahmed</h2>
-                  <h3 className="text-md font-medium">Web Developer</h3>
-                  <div className="flex gap-3 mt-3">
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <RiFacebookFill className="text-md" />
-                    </p>
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <FaInstagram className="text-md" />
-                    </p>
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <FaLinkedinIn className="text-md" />
-                    </p>
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <FaTwitter className="text-md" />
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="flex items-center gap-4">
-                <img
-                  className="w-28 h-28 rounded-full"
-                  src="https://demo.egenslab.com/html/eventlab/assets/images/speaker/speaker-sm1.png"
-                  alt=""
-                />
-                <div>
-                  <h2 className="text-xl font-semibold">Asif Mahmud</h2>
-                  <h3 className="text-md font-medium">Graphic Designer</h3>
-                  <div className="flex gap-3 mt-3">
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <RiFacebookFill className="text-md" />
-                    </p>
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <FaInstagram className="text-md" />
-                    </p>
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <FaLinkedinIn className="text-md" />
-                    </p>
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <FaTwitter className="text-md" />
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="flex items-center gap-4">
-                <img
-                  className="w-28 h-28 rounded-full"
-                  src="https://demo.egenslab.com/html/eventlab/assets/images/speaker/speaker-sm2.png"
-                  alt=""
-                />
-                <div>
-                  <h2 className="text-xl font-semibold">Arif Hossain</h2>
-                  <h3 className="text-md font-medium">Digital Marketer</h3>
-                  <div className="flex gap-3 mt-3">
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <RiFacebookFill className="text-md" />
-                    </p>
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <FaInstagram className="text-md" />
-                    </p>
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <FaLinkedinIn className="text-md" />
-                    </p>
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <FaTwitter className="text-md" />
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="space-y-10">
-              <div className="flex items-center gap-4">
-                <img
-                  className="w-28 h-28 rounded-full"
-                  src="https://1.bp.blogspot.com/-HoyT-d9JX0g/YUtRefQcHLI/AAAAAAAAk9w/P-NTrf0jPColdcKjKy2XGAVAjLjbQUyIQCLcBGAsYHQ/s16000-rw/50%2B%252B%2BIndian%2BHijab%2BGirl%2BDP%2Bfor%2BInstagram%2BPage%2Bwith%2BCaption%2B%252825%2529.jpg"
-                  alt=""
-                />
-                <div>
-                  <h2 className="text-xl font-semibold">Sabrina Suraiya</h2>
-                  <h3 className="text-md font-medium">Product Designer</h3>
-                  <div className="flex gap-3 mt-3">
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <RiFacebookFill className="text-md" />
-                    </p>
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <FaInstagram className="text-md" />
-                    </p>
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <FaLinkedinIn className="text-md" />
-                    </p>
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <FaTwitter className="text-md" />
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="flex items-center gap-4">
-                <img
-                  className="w-28 h-28 rounded-full"
-                  src="https://demo.egenslab.com/html/eventlab/assets/images/speaker/speaker-sm4.png"
-                  alt=""
-                />
-                <div>
-                  <h2 className="text-xl font-semibold">Lutfur Rahman</h2>
-                  <h3 className="text-md font-medium">
-                    Brand Identity Designer
-                  </h3>
-                  <div className="flex gap-3 mt-3">
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <RiFacebookFill className="text-md" />
-                    </p>
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <FaInstagram className="text-md" />
-                    </p>
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <FaLinkedinIn className="text-md" />
-                    </p>
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <FaTwitter className="text-md" />
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="flex items-center gap-4">
-                <img
-                  className="w-28 h-28 rounded-full"
-                  src="https://i.ibb.co/pPtyQxm/360-F-214746128-31-Jkea-P6r-U0-Nzzzd-FC4kh-Gkmqc8noe6h.jpg"
-                  alt=""
-                />
-                <div>
-                  <h2 className="text-xl font-semibold">Rizve Ahmed</h2>
-                  <h3 className="text-md font-medium">Writer</h3>
-                  <div className="flex gap-3 mt-3">
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <RiFacebookFill className="text-md" />
-                    </p>
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <FaInstagram className="text-md" />
-                    </p>
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <FaLinkedinIn className="text-md" />
-                    </p>
-                    <p className="border-2 border-gray-500 rounded-full p-1">
-                      <FaTwitter className="text-md" />
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
         <div>
           <div className="bg-base-200 py-5 lg:w-96">
@@ -417,28 +215,6 @@ const EventDetails = () => {
                   </div>
                 </div>
               ))}
-            </div>
-            <div className="grid md:grid-cols-2 lg:grid-cols-1">
-              <div className="bg-base-200 py-5 lg:w-96 mt-8 lg:mt-10">
-                <h2 className="bg-pink-700 px-4 py-2 text-2xl w-72 text-center text-white rounded-r-md flex items-center gap-2">
-                  <MdPersonAddAlt1 className="text-3xl" />
-                  Event Organized By
-                </h2>
-                <div className="ml-32 mt-6">
-                  <img
-                    className="w-40 h-40 rounded-md"
-                    src="https://i.ibb.co/c2PW68Q/pexels-italo-melo-2379004.jpg"
-                    alt=""
-                  />
-                  <div className="mt-5 ">
-                    <h2 className="text-2xl font-semibold">Abdullah Mamun</h2>
-                    <h3 className="ml-16 text-lg">CEO</h3>
-                  </div>
-                </div>
-              </div>
-              <div className="mt-8 px-2">
-                <img src="https://i.ibb.co/gzYpGCB/sb-banner.png" alt="" />
-              </div>
             </div>
           </div>
         </div>

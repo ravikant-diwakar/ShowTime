5f33078450b86e909b680827cb5edd2c5bb4a6c3
Gauravkumar1741 <135353200+Gauravkumar1741@users.noreply.github.com>
2024-07-25 20:34:28 +0530

    Update useAuth.jsx

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { AuthContext } from "../authProvider/AuthProvider";
+import { AuthContext } from "../AuthProvider/AuthProvider";
 
 const useAuth = () => {
     const userInfo = useContext(AuthContext)
